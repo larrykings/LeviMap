b0cdbb56f04f68dccb9a8e41ed0a1f30dc5aaaa0
larrykings <84679942+larrykings@users.noreply.github.com>
2023-02-07 19:02:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const App = () => {
       const popupOffset = {
         bottom: [0, -25]
       }
-      const popup = new tt.Popup({ offset: popupOffset }).setHTML('Hello Alex let's go😎!')
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('Hello Alex let's go.😎!')
       const element = document.createElement('div')
       element.className = 'marker'
 
